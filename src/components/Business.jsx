14745e403476be8320a03db640fb4369ce1eea1f
Zafeer Mahmood <zafeer7460@gmail.com>
2023-07-07 17:53:07 +0500

    added billing styles and feature styles

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,6 +1,22 @@
 import { features } from "../constants";
 import style, { layout } from "../style";
 import { Button } from "./Button";
+
+
+
+const FeatureCard = ({icon,title,content,index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index!==features.length-1 ? "mb-6": "mb-0" } feature-card `}>
+      <div className={`w-[64px] h-[64px] rounded-full ${style.flexCenter} bg-dimBlue `}>
+        <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+      </div>
+      <div className="flex-1 flex flex-col ml-3">
+        <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">{title}</h4>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">{content}</p>
+      </div>
+  </div>
+)
+
+
 export const Business = () => {
   return (
     <section className={`${layout.section}`} id="features">
@@ -14,7 +30,13 @@ export const Business = () => {
           building credit,earning rewards and saving money. But with the
           hundreds of credit card on the market.
         </p>
-        <Button styles="mt-10" text={"Get Started"} />
+        <Button styles="mt-10" text={"Get Started"}  />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature,index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}></FeatureCard>
+        ))}
       </div>
     </section>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,13 @@
 
-export const Button = ({styles , text}) => {
+
+export const Button = ({styles,text}) => {
+
   return (
     <button type= 'button' className={` ${styles} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none`}>{text}</button>
   )
+  
+}
+Button.propTypes = {
+  styles: " ",
+  text: " "
 }

05a1882999f7ca122b648825475189a23e0f4cbe
Zafeer Mahmood <zafeer7460@gmail.com>
2023-07-10 22:33:38 +0500

    completed working on CardDeal.jsx and still working on Testimonial.jsx

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,6 +1,24 @@
+import {card} from '../assets'
+import styles,{layout} from '../style'
+import {Button} from './Button'
+
+
 
 export const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal
+          <br className="sm:block hidden" /> in few easy steps.
+          <p className={`${styles.paragraph} max-w-md mt-5`}>
+            Arcu tortor, purus in mattis at sed integer facuibus. Aliquent quis aliquet eget mautis tortor quisque.
+          </p>
+        </h2>
+        <Button styles={'mt-10'} text={'Get Started'}/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-full h-full" />
+      </div>
+    </section>
   )
 }
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,14 @@
-
+import {feedback} from '../constants'
+import styles, { layout } from "../style";
+import Feedback from './Feedback'
 export const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`} id='clients' >
+      {/* {TODO} */}
+      <div className=""></div>
+      <div className='w-full flex justify-center items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-10'>
+
+      </div>
+    </section>
   )
 }
